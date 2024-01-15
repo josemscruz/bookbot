65d6cdd08ae4653767223f07a85c75ca304c1873
root <root@DESKTOP-OUUC0KR>
2024-01-15 19:25:51 +0000

    Added count of characters

---
+++ main.py
@@ -0,0 +1,24 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    letters = {}
+    for i in range(len(text)):
+        char = ""
+        if text[i].isalpha():
+            char = text[i].lower()
+        if char not in letters:
+            letters[char] = 1
+        letters[char] += 1
+    print(letters)
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
