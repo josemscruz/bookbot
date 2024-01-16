89b55f0e34df50fabcaf1f68e7a1ffc5e12f99f3
root <root@DESKTOP-OUUC0KR>
2024-01-16 17:49:06 +0000

    got help from the ai to fix the ordering of the dicts, and also removed the empty string from the count

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
+    num_words = get_num_words(text)
     letters = {}
     for i in range(len(text)):
         char = ""
         if text[i].isalpha():
             char = text[i].lower()
-        if char not in letters:
-            letters[char] = 1
-        else:
-            letters[char] += 1
-    print(letters)
+            if char not in letters:
+                letters[char] = 1
+            else:
+                letters[char] += 1
 
+    sorted_letters = dict(sorted(letters.items(), key = lambda item: item[1], reverse=True))
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document\n")
+    for l in sorted_letters:
+        print(f"The '{l}' character was found {letters[l]} times")
+    print("--- End report ---")
+    
 def get_num_words(text):
     words = text.split()
     return len(words)
